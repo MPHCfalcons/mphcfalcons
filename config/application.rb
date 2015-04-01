9f81266ddb4e008a2401b10d38325560c222fe98
hippo240 <lostsoul80@live.com.au>
2015-04-01 10:41:57 +0000

    CSS fix? 3

--- config/application.rb
+++ config/application.rb
@@ -67,11 +67,10 @@ module Mphc
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.precompile = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.initialize_on_precompile = true
+    config.assets.initialize_on_precompile = false
   end
 end
